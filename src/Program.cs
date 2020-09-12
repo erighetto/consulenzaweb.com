0701aa941d628d5e6b682b9035e07cd0e4d8aafd
Emanuel Righetto <posta@emanuelrighetto.it>
2020-09-12 11:26:46 +0200

    initial commmit

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Web;
+
+namespace ConsulenzaWeb
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .RunAsync();
+    }
+}
